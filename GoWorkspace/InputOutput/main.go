50ad5a1b158b14bac88c21e213e57562f0a88046
Paul Tobin <tobster111@gmail.com>
2024-03-16 10:57:13 +0000

    Add birthday function to increment age

--- GoWorkspace/InputOutput/main.go
+++ GoWorkspace/InputOutput/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // global variable
 var url = "https://www.google.com"
 
@@ -9,43 +11,50 @@ func calculateTax(price float32) (float32, float32) {
 	return price * 0.075, price * 0.025
 }
 
+func birthday(age int)  {
+	age+= 1
+}
+
 func main() {
-	stateTax, _ := calculateTax(100)
+	// stateTax, _ := calculateTax(100)
 
-	println(stateTax)
 
-	println("Hello World!")
-	var x int = 5
-	println(x)
-	var name string = "Paul"
-	println(name)
-	const y = 10
-	println(y)
+	// println(stateTax)
 
-	// function scope
-	var text string = "Hello"
-	println(text)
+	// println("Hello World!")
+	// var x int = 5
+	// println(x)
+	// var name string = "Paul"
+	// println(name)
+	// const y = 10
+	// println(y)
 
-	otherText := "World"
-	println(otherText)
+	// // function scope
+	// var text string = "Hello"
+	// println(text)
 
-	otherNumber := 10
-	println(otherNumber)
+	// otherText := "World"
+	// println(otherText)
 
-	println(url)
+	// otherNumber := 10
+	// println(otherNumber)
 
-	PrintData()
+	// println(url)
+
+	// PrintData()
 
 	id := 1
 	firstName := "Paul"
 	age := 25
+	birthday(age)
+	fmt.Println(age)
 	println(id, firstName, age)
 
 	// Collections
-	var numbers [5]int
+	// var numbers [5]int
 
 	// Slices: similar to a dynamic length array, but they are actually chunks of an array
-	var names []string
+	// var names []string
 
 
 }

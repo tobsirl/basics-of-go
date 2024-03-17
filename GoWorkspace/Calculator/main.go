02dfc08fbf2fab78c83fb956555baded8f86caa5
Paul Tobin <tobster111@gmail.com>
2024-03-17 09:31:36 +0000

    Add basic calculator functionality

---
+++ GoWorkspace/Calculator/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var operation string
+	var number1 int
+	var number2 int
+
+	fmt.Println("Calculator Go 1.0")
+	fmt.Println("<====================>")
+	fmt.Println("Enter first number: ")
+	fmt.Scanln(&number1)
+	fmt.Println("Enter second number: ")
+	fmt.Scanln(&number2)
+	fmt.Println("Enter operation: ")
+	fmt.Scanln(&operation)
+}

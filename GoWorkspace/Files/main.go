a770a05544cec0f2ad0af93cc9142a85a4e72cc1
Paul Tobin <tobster111@gmail.com>
2024-03-19 17:18:34 +0000

    Add content to main.go and create text.txt.output.txt

--- GoWorkspace/Files/main.go
+++ GoWorkspace/Files/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	fileutils "frontendmasters.com/go/files/fileutils"
@@ -14,7 +15,7 @@ func main() {
 	if err != nil {
 		println("Error reading file")
 	} else {
-		newContent	:= c + "Appended text"
+		newContent	:= fmt.Sprintf("Original: %v\n Double Original: %v%v", c, c, c)
 		println("File content: ", c)
 		fileutils.WriteToFile(filePath + ".output.txt", newContent)
 	}

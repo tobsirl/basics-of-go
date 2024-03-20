560b7e6e2cdbbcd56930207a8988c64c309e5fe9
Paul Tobin <tobster111@gmail.com>
2024-03-20 18:27:15 +0000

    Add toKm function to convert miles to kilometers

--- GoWorkspace/Files/data/types.go
+++ GoWorkspace/Files/data/types.go
@@ -6,6 +6,11 @@ type json = map[string]string
 var x integer
 
 type distance float64
+type distanceKm	float64
+
+func toKm(miles distance) distanceKm {
+	return distanceKm(1.60934 * miles)
+}
 
 func test() {
 	d := distance(42.42)

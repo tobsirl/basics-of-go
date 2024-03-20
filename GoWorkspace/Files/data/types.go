2b922197c4ee3dabfa008ba8fb71194012ad81a9
Paul Tobin <tobster111@gmail.com>
2024-03-20 18:31:30 +0000

    Add distance conversion methods

--- GoWorkspace/Files/data/types.go
+++ GoWorkspace/Files/data/types.go
@@ -8,11 +8,19 @@ var x integer
 type distance float64
 type distanceKm	float64
 
-func toKm(miles distance) distanceKm {
+// Method to convert distance from miles to km
+func (miles distance) ToKm() distanceKm {
 	return distanceKm(1.60934 * miles)
+}
+
+// Method to convert distance from km to miles
+func (km distanceKm) ToMiles() distance {
+	return distance(km / 1.60934)
 }
 
 func test() {
 	d := distance(42.42)
-	println(d)
+	km := d.ToKm()
+	km.ToMiles()
+	println(km)
 }

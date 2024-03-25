bd3d8ae0a4c9d23cdff60403c55067cff81f11fa
Paul Tobin <tobster111@gmail.com>
2024-03-25 18:24:02 +0000

    Add NewWorkShop function to create a Workshop instance

--- GoWorkspace/FEM/data/workshop.go
+++ GoWorkspace/FEM/data/workshop.go
@@ -6,3 +6,13 @@ type Workshop struct {
 	Course // embedding
 	Date  time.Time
 }
+
+func NewWorkShop(name string, instructor Instructor) Workshop {
+	return Workshop{
+		Course: Course{
+			Name:       name,
+			Instructor: instructor,
+		},
+		Date: time.Now(),
+	}
+}
